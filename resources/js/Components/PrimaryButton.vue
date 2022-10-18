ff34421c80e7a160cae44ab15535fa3b73bf8d00
SAI KWARN KHAM <kwarnkham.moon@gmail.com>
2022-10-18 12:59:24 +0630

    style the forms

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -2,13 +2,16 @@
 defineProps({
     type: {
         type: String,
-        default: 'submit',
+        default: "button",
     },
 });
 </script>
 
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+    <button
+        :type="type"
+        class="daisy-btn daisy-btn-primary daisy-btn-sm capitalize"
+    >
         <slot />
     </button>
 </template>
---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,7 @@
+<script></script>
+
+<template>
+    <div class="h-screen w-screen flex flex-col" data-theme="garden">
+        <slot />
+    </div>
+</template>
