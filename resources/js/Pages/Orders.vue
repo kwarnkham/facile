10bde11da066b43d35c45f90f1bf7fe10d595679
KWARN KHAM <kwarnkham.moon@gmail.com>
2022-12-19 17:20:07 +0630

    handle empty data

--- resources/js/Pages/Orders.vue
+++ resources/js/Pages/Orders.vue
@@ -12,10 +12,12 @@ const props = defineProps({
     <div class="h-full overflow-y-auto p-1">
         <Head title="Orders" />
         <div
+            v-if="orders.data.length"
             v-for="order in orders.data"
             @click="$inertia.visit(route('orders.show', { order: order.id }))"
         >
             {{ order.id }} : {{ order.amount - order.discount }}
         </div>
+        <div v-else class="text-center text-lg font-bold">No order yet</div>
     </div>
 </template>
