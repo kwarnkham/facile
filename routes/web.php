0def28d5b3f5f6a5f17c2995b5355cd9abcd8e15
KWARN KHAM <kwarnkham.moon@gmail.com>
2023-04-07 10:20:07 +0630

    use welcome view

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Application;
-use Inertia\Inertia;
 
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return view('welcome');
 })->name('index');
 
 
